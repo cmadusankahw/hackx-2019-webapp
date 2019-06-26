8b0ae4ca3ab0f10568dd7d891fd551c9c3b58932
cmadusankahw <cmadusankahw@gmail.com>
2019-06-26 08:50:03 +0530

    counter update 2

--- js/main.js
+++ js/main.js
@@ -76,7 +76,7 @@ setInterval(function() {
 
    if ($('.countdown').length > 0) {
       $(".countdown").jCounter({
-         date: '25 September 2019 12:00:00',
+         date: '25 August 2019 12:00:00',
          fallback: function () {
             console.log("count finished!")
          }
